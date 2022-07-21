88cb9e0de9dcaf8a827736c19ae7da2e57a26f86
ngasimzade <nabat.gasimzade@pashabank.az>
2022-07-21 11:15:49 +0400

    adding Test

---
+++ src/main/java/az/pashabank/internship/demo/test/Test.java
@@ -0,0 +1,8 @@
+package az.pashabank.internship.demo.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        System.out.println("Nadir");
+    }
+}
